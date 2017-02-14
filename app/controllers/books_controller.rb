faacd0bd29b4b88bccfb440842e89744b63688f6
forrest <743759846@qq.com>
2017-02-14 22:09:06 +0800

    cannot add same item in cart

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,12 @@ class BooksController < ApplicationController
 
   def add_to_basket
     @book = Book.find(params[:id])
-    current_basket.add_book_to_basket(@book)
-    flash[:notice] = "成功加入漂流瓶"
+   if !current_basket.books.include?(@book)
+     current_basket.add_book_to_basket(@book)
+     flash[:notice] = "你已成功将 #{@book.title} 加入购物车"
+   else
+     flash[:warning] = "你的购物车内已有此物品"
+   end
     redirect_to :back
   end
 end
