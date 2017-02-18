9ba610d753e0ac9616b80670552c9263701505da
forrest <743759846@qq.com>
2017-02-18 10:21:03 +0800

    fix category bookLists show hidden

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -2,7 +2,7 @@ class CategorysController < ApplicationController
 
   def show
     @category = Category.find_by_name(params[:id])
-    @books = @category.books
+    @books = @category.books.published.recent
   end
 
 end
