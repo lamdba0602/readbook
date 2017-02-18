8b0260b5f9b242aae4315ba03dc58c38e1cf6f59
forrest <743759846@qq.com>
2017-02-18 22:51:07 +0800

    fix ifram in heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_p3p
+
+  def set_p3p
+    response.headers["P3P"]='CP="CAO PSA OUR"'
+  end
  
   def admin_required
     if !current_user.admin?
