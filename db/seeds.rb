5c26c7e8a08fac0aacd5185123fbf16cc9da5dd6
forrest <743759846@qq.com>
2017-02-18 14:36:30 +0800

    fix footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-category = [ "文学", "科学", "小说", "心理学", "艺术" ]
+category = ["历史", "文学", "科学", "小说", "心理学", "艺术" ]
 
 category.each do |f|
   Category.create!(name: f)
